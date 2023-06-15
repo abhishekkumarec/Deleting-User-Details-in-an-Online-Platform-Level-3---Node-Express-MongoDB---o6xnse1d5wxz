8b887f0ff738ce34b48c0f7bcf28c182c7a9ceb8
Abhishek Kumar <126902053+abhishekkumarec@users.noreply.github.com>
2023-06-15 18:31:17 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,99 +1,41 @@
-const fs = require("fs");
-const express = require("express");
+const express = require('express');
 const app = express();
+const userDetails = require('./data/userDetails.json');
 
-// Importing products from userDetails.json file
-const userDetails = JSON.parse(
-  fs.readFileSync(`${__dirname}/data/userDetails.json`)
-);
-
-//Middlewares
-app.use(express.json());
-
-//Write DELETE endpoint for deleting the details of user
-
-// PATCH endpoint for editing user details
-app.patch("/api/v1/details/:id", (req, res) => {
-  const id = req.params.id * 1;
-  const updatedDetails = userDetails.find(
-    (updatedDetails) => updatedDetails.id === id
-  );
-  const index = userDetails.indexOf(updatedDetails);
-  if (!updatedDetails) {
-    return res.status(404).send({
-      status: "failed",
-      message: "User not found!",
+// Route to delete a user detail
+app.delete('/api/v1/details/:id', (req, res) => {
+  // Get the id of the user to be deleted
+  const userId = req.params.id;
+  
+  // Find the user with the given id
+  const user = userDetails.find((user) => user.id === parseInt(userId));
+  
+  // If the user is found, remove the user object from the userDetails array
+  if (user) {
+    const index = userDetails.indexOf(user);
+    userDetails.splice(index, 1);
+    
+    // Return a JSON response with a 200 OK status code along with a success message and the deleted user detail
+    return res.status(200).json({
+      status: 'success',
+      message: 'User details deleted successfully',
+      data: {
+        details: user
+      }
     });
   }
-
-  Object.assign(updatedDetails, req.body);
-
-  fs.writeFile(
-    `${__dirname}/data/userDetails.json`,
-    JSON.stringify(userDetails),
-    (err) => {
-      res.status(200).json({
-        status: "success",
-        message: `User details updated successfully for id: ${updatedDetails.id}`,
-        data: {
-          updatedDetails,
-        },
-      });
-    }
-  );
-});
-
-// POST endpoint for registering new user
-app.post("/api/v1/details", (req, res) => {
-  const newId = userDetails[userDetails.length - 1].id + 1;
-  const { name, mail, number } = req.body;
-  const newUser = { id: newId, name, mail, number };
-  userDetails.push(newUser);
-  fs.writeFile(
-    `${__dirname}/data/userDetails.json`,
-    JSON.stringify(userDetails),
-    (err) => {
-      res.status(201).json({
-        status: "Success",
-        message: "User registered successfully",
-        data: {
-          userDetails: newUser,
-        },
-      });
-    }
-  );
-});
-
-// GET endpoint for sending the details of users
-app.get("/api/v1/details", (req, res) => {
-  res.status(200).json({
-    status: "Success",
-    message: "Detail of users fetched successfully",
-    data: {
-      userDetails,
-    },
+  
+  // If the user is not found, return a JSON response with a 404 Not Found status code along with an error message
+  return res.status(404).json({
+    status: 'failed',
+    message: 'User not found!'
   });
 });
 
-// GET endpoint for sending the details of users by id
-app.get("/api/v1/userdetails/:id", (req, res) => {
-  let { id } = req.params;
-  id *= 1;
-  const details = userDetails.find((details) => details.id === id);
-  if (!details) {
-    return res.status(404).send({
-      status: "failed",
-      message: "User not found!",
-    });
-  } else {
-    res.status(200).send({
-      status: "success",
-      message: "Details of users fetched successfully",
-      data: {
-        details,
-      },
-    });
-  }
+// Start the server
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
 });
 
-module.exports = app;
+
+module.exports = app;
